d6e4ad62ae180d1bc0e6ef51ed71f261680c882a
Percy <maldonadoq.percy@gmail.com>
2019-09-19 11:34:05 -0500

    upload photo [incomplete]

--- app/src/main/java/maldonado/indetect/MainActivity.kt
+++ app/src/main/java/maldonado/indetect/MainActivity.kt
@@ -1,9 +1,12 @@
 package maldonado.indetect
 
+import android.app.Activity
 import android.app.Dialog
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.os.Build
 import android.os.Bundle
-import android.text.method.ScrollingMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.Window
@@ -15,39 +18,39 @@ import androidx.appcompat.app.AppCompatActivity
 import com.wonderkiln.camerakit.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var textViewResult: TextView
     lateinit var btnDetectObject: Button
     lateinit var btnToggleCamera: Button
     lateinit var btnUploadPhoto:  Button
-    lateinit var imageViewResult: ImageView
     lateinit var cameraView: CameraView
+    lateinit var imageViewTmp: ImageView
+
+    lateinit var ivImageResult: ImageView
+    lateinit var tvLoadingText: TextView
+    lateinit var tvTextResults: TextView
+    lateinit var aviLoaderHolder: View
+    lateinit var resultDialog: Dialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         cameraView = findViewById(R.id.cameraView)
-        imageViewResult = findViewById(R.id.imageViewResult)
-        textViewResult = findViewById(R.id.textViewResult)
-        textViewResult.movementMethod = ScrollingMovementMethod()
-
+        imageViewTmp = ImageView(this)
         btnToggleCamera = findViewById(R.id.btnToggleCamera)
         btnDetectObject = findViewById(R.id.btnDetectObject)
         btnUploadPhoto = findViewById(R.id.btnUploadPhoto)
 
-        val resultDialog = Dialog(this)
+        resultDialog = Dialog(this)
         val customProgressView = LayoutInflater.from(this).inflate(R.layout.activity_result, null)
         resultDialog.setCancelable(false)
         resultDialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
         resultDialog.setContentView(customProgressView)
 
-        val ivImageResult = customProgressView.findViewById<ImageView>(R.id.iViewResult)
-        val tvLoadingText = customProgressView.findViewById<TextView>(R.id.tvLoadingRecognition)
-        val tvTextResults = customProgressView.findViewById<TextView>(R.id.tvResult)
-
-
+        ivImageResult = customProgressView.findViewById(R.id.iViewResult)
+        tvLoadingText = customProgressView.findViewById(R.id.tvLoadingRecognition)
+        tvTextResults = customProgressView.findViewById(R.id.tvResult)
         // The Loader Holder is used due to a bug in the Avi Loader library
-        val aviLoaderHolder = customProgressView.findViewById<View>(R.id.aviLoaderHolderView)
+        aviLoaderHolder = customProgressView.findViewById<View>(R.id.aviLoaderHolderView)
 
         cameraView.addCameraKitListener(object : CameraKitEventListener {
             override fun onEvent(cameraKitEvent: CameraKitEvent) { }
@@ -55,22 +58,7 @@ class MainActivity : AppCompatActivity() {
             override fun onError(cameraKitError: CameraKitError) { }
 
             override fun onImage(cameraKitImage: CameraKitImage) {
-
-                var bitmap = cameraKitImage.bitmap
-                bitmap = Bitmap.createScaledBitmap(bitmap, INPUT_SIZE, INPUT_SIZE, false)
-
-                aviLoaderHolder.visibility = View.GONE
-                tvLoadingText.visibility = View.GONE
-
-                val results = "Sabpe"
-                ivImageResult.setImageBitmap(bitmap)
-                tvTextResults.text = results
-
-                tvTextResults.visibility = View.VISIBLE
-                ivImageResult.visibility = View.VISIBLE
-
-                resultDialog.setCancelable(true)
-
+                recognize(cameraKitImage.bitmap)
             }
 
             override fun onVideo(cameraKitVideo: CameraKitVideo) { }
@@ -86,13 +74,42 @@ class MainActivity : AppCompatActivity() {
         }
 
         btnUploadPhoto.setOnClickListener {
-            Toast.makeText(this, "Hi there! Saluuute.", Toast.LENGTH_LONG).show()
+            //Toast.makeText(this, "Hi there! Saluuute.", Toast.LENGTH_LONG).show()
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if(checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_DENIED){
+                    val permissions = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                    requestPermissions(permissions, PERMISSION_CODE)
+                }
+                else{
+                    pickImageFromGalley()
+                }
+            }
+            else{
+                pickImageFromGalley()
+            }
         }
 
         resultDialog.setOnDismissListener {
             tvLoadingText.visibility = View.VISIBLE
             aviLoaderHolder.visibility = View.VISIBLE
         }
+    }
+
+    private fun recognize(tbitmap: Bitmap) {
+        var bitmap = Bitmap.createScaledBitmap(tbitmap, INPUT_SIZE, INPUT_SIZE, false)
+
+        aviLoaderHolder.visibility = View.GONE
+        tvLoadingText.visibility = View.GONE
+
+        val results = "Sabpe"
+        ivImageResult.setImageBitmap(bitmap)
+        tvTextResults.text = results
+
+        tvTextResults.visibility = View.VISIBLE
+        ivImageResult.visibility = View.VISIBLE
+
+        resultDialog.setCancelable(true)
     }
 
     override fun onResume() {
@@ -108,5 +125,41 @@ class MainActivity : AppCompatActivity() {
     companion object {
         // private const val LABEL_PATH = "labels.txt"
         private const val INPUT_SIZE = 300
+        private const val IMAGE_PICK_CODE = 1000
+        private const val PERMISSION_CODE = 1001
+    }
+
+    private fun pickImageFromGalley() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+
+    // Pick Image Functions
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            PERMISSION_CODE -> {
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    pickImageFromGalley()
+                }
+                else{
+                    Toast.makeText(this, "Bukake", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        // super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+            imageViewTmp.setImageURI(data?.data)
+            // recognize(imageViewTmp.drawable)
+        }
     }
 }

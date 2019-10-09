64b41ddcba9b57bbf602489fb05124e86b674e8c
Maldonado <maldonadoq.percy@gmail.com>
2019-10-09 17:31:03 -0500

    models

--- app/src/main/java/maldonado/indetect/MainActivity.kt
+++ app/src/main/java/maldonado/indetect/MainActivity.kt
@@ -1,12 +1,7 @@
 package maldonado.indetect
 
-import android.app.Activity
 import android.app.Dialog
-import android.content.Intent
-import android.content.pm.PackageManager
 import android.graphics.*
-import android.graphics.drawable.BitmapDrawable
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -19,11 +14,8 @@ import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
     private lateinit var btnDetectObject: Button
-    private lateinit var btnDetectCar: Button
-    private lateinit var btnDetectFlower: Button
 
     private lateinit var btnToggleCamera: Button
-    private lateinit var btnUploadPhoto:  Button
     private lateinit var cameraView: CameraView
     private lateinit var imageViewTmp: ImageView
 
@@ -38,8 +30,6 @@ class MainActivity : AppCompatActivity() {
 
     // model
     private lateinit var objectClassifier: ObjectClassifier
-    private lateinit var carClassifier: CarClassifier
-    private lateinit var flowerClassifier: FlowerClassifier
 
     private val executor = Executors.newSingleThreadExecutor()
 
@@ -50,11 +40,7 @@ class MainActivity : AppCompatActivity() {
         cameraView = findViewById(R.id.cameraView)
         imageViewTmp = ImageView(this)
         btnToggleCamera = findViewById(R.id.btnToggleCamera)
-        btnUploadPhoto = findViewById(R.id.btnUploadPhoto)
-
         btnDetectObject = findViewById(R.id.btnDetectObject)
-        btnDetectCar = findViewById(R.id.btnDetectCar)
-        btnDetectFlower = findViewById(R.id.btnDetectFlower)
 
         resultDialog = Dialog(this)
         val customProgressView = LayoutInflater.from(this).inflate(R.layout.activity_result,
@@ -97,42 +83,6 @@ class MainActivity : AppCompatActivity() {
             ivImageResult.visibility = View.GONE
         }
 
-        btnDetectCar.setOnClickListener {
-            btnType = 2
-            tvLoadingText.text = "Car Identification Engine Processing ..."
-
-            cameraView.captureImage()
-            resultDialog.show()
-            tvTextResults.visibility = View.GONE
-            ivImageResult.visibility = View.GONE
-        }
-
-        btnDetectFlower.setOnClickListener {
-            btnType = 3
-            tvLoadingText.text = "Flower Identification Engine Processing ..."
-
-            cameraView.captureImage()
-            resultDialog.show()
-            tvTextResults.visibility = View.GONE
-            ivImageResult.visibility = View.GONE
-        }
-
-        btnUploadPhoto.setOnClickListener {
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                if(checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) ==
-                    PackageManager.PERMISSION_DENIED){
-                    val permissions = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE)
-                    requestPermissions(permissions, PERMISSION_CODE)
-                }
-                else{
-                    pickImageFromGalley()
-                }
-            }
-            else{
-                pickImageFromGalley()
-            }
-        }
-
         resultDialog.setOnDismissListener {
             tvLoadingText.visibility = View.VISIBLE
             aviLoaderHolder.visibility = View.VISIBLE
@@ -146,19 +96,7 @@ class MainActivity : AppCompatActivity() {
         aviLoaderHolder.visibility = View.GONE
         tvLoadingText.visibility = View.GONE
 
-        var results = ArrayList<IClassifier.Recognition>()
-
-        when(btnType){
-            1 -> {
-                results = objectClassifier.recognizeImage(bitmap)
-            }
-            2 -> {
-                results = carClassifier.recognizeImage(bitmap)
-            }
-            3 -> {
-                results = flowerClassifier.recognizeImage(bitmap)
-            }
-        }
+        val results = objectClassifier.recognizeImage(bitmap)
 
         val canvas = Canvas(bitmap)
         val boxPaint = Paint()
@@ -170,14 +108,12 @@ class MainActivity : AppCompatActivity() {
         textPaint.textSize = 50.0f
 
         for (result in results) {
-            if(!result.location.isEmpty){
-                boxPaint.color = Color.argb(255, random.nextInt(256), random.nextInt(
-                    256), random.nextInt(256))
-                canvas.drawRoundRect(result.location, 30.0f, 30.0f, boxPaint)
+            boxPaint.color = Color.argb(255, random.nextInt(256), random.nextInt(
+                256), random.nextInt(256))
+            canvas.drawRoundRect(result.location, 30.0f, 30.0f, boxPaint)
 
-                canvas.drawText(String.format("%s %.2f", result.title, (100 * result.confidence)),
-                    result.location.left + 40, result.location.top + 60, textPaint)
-            }
+            canvas.drawText(String.format("%s %.2f", result.title, (100 * result.confidence)),
+                result.location.left + 40, result.location.top + 60, textPaint)
         }
 
         ivImageResult.setImageBitmap(bitmap)
@@ -199,62 +135,16 @@ class MainActivity : AppCompatActivity() {
         super.onPause()
     }
 
-    companion object {
-        private const val IMAGE_PICK_CODE = 1000
-        private const val PERMISSION_CODE = 1001
-    }
-
-    private fun pickImageFromGalley() {
-        val intent = Intent(Intent.ACTION_PICK)
-        intent.type = "image/*"
-        startActivityForResult(intent, IMAGE_PICK_CODE)
-    }
-
-
-    // Pick Image Functions
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when(requestCode){
-            PERMISSION_CODE -> {
-                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    pickImageFromGalley()
-                }
-            }
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        // super.onActivityResult(requestCode, resultCode, data)
-        if(resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
-            imageViewTmp.setImageURI(data?.data)
-
-            btnType = 1
-            val bt = (imageViewTmp.drawable as BitmapDrawable).bitmap
-            recognize(Bitmap.createScaledBitmap(bt, (bt.width*0.5).toInt(), (bt.height*0.5).toInt(),
-                false))
-
-            resultDialog.show()
-        }
-    }
-
     // tensor
     override fun onDestroy() {
         super.onDestroy()
         executor.execute { objectClassifier.close() }
-        executor.execute { carClassifier.close() }
-        executor.execute { flowerClassifier.close() }
     }
 
     private fun initTensorFlowAndLoadModel() {
         executor.execute {
             try {
                 objectClassifier = ObjectClassifier.create(assets)
-                carClassifier = CarClassifier.create(assets)
-                flowerClassifier = FlowerClassifier.create(assets)
 
                 makeButtonVisible()
             } catch (e: Exception) {

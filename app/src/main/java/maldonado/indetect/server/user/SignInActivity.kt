7fd33a54cf18e4941ed7214c4e1373eb28664e89
Maldonado <maldonadoq.percy@gmail.com>
2019-10-25 12:10:03 -0500

    firebase incomplete, we can't create user table!

--- app/src/main/java/maldonado/indetect/server/user/SignInActivity.kt
+++ app/src/main/java/maldonado/indetect/server/user/SignInActivity.kt
@@ -1,13 +1,67 @@
 package maldonado.indetect.server.user
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import maldonado.indetect.R
+import maldonado.indetect.server.ServerActivity
 
 class SignInActivity : AppCompatActivity() {
+    private lateinit var txtEmail: EditText
+    private lateinit var txtPassword: EditText
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        txtEmail = findViewById(R.id.txtEmail)
+        txtPassword = findViewById(R.id.txtPassword)
+
+        auth = FirebaseAuth.getInstance()
+    }
+
+    fun signIn(view: View){
+        loginUser()
+    }
+
+    fun forgot(view: View){
+
+    }
+
+    fun register(view: View){
+        intent = Intent(this@SignInActivity, SignUpActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun loginUser(){
+        val email:String = txtEmail.text.toString()
+        val password:String = txtPassword.text.toString()
+
+        if(email.isNotEmpty() and password.isNotEmpty()){
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this){
+                    task ->
+                    if(task.isComplete){
+                        action()
+                    }
+                    else{
+                        Toast.makeText(this, "Authentication Error!", Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
+        else{
+            Toast.makeText(this, "Fields can't be empty!", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun action(){
+        intent = Intent(this@SignInActivity, ServerActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/maldonado/indetect/server/user/SignUpActivity.kt
+++ app/src/main/java/maldonado/indetect/server/user/SignUpActivity.kt
@@ -1,13 +1,88 @@
 package maldonado.indetect.server.user
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import maldonado.indetect.R
+import maldonado.indetect.server.ServerActivity
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var txtName: EditText
+    private lateinit var txtEmail: EditText
+    private lateinit var txtPassword: EditText
+
+    private lateinit var dbReference: DatabaseReference
+    private lateinit var database: FirebaseDatabase
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        txtName = findViewById(R.id.txtName)
+        txtEmail = findViewById(R.id.txtEmail)
+        txtPassword = findViewById(R.id.txtPassword)
+
+        database = FirebaseDatabase.getInstance()
+        auth = FirebaseAuth.getInstance()
+
+        dbReference = database.reference.child("User")
+    }
+
+    fun signUp(view: View) {
+        createAccount()
+    }
+
+    private fun createAccount(){
+        val name:String = txtName.text.toString()
+        val email:String = txtEmail.text.toString()
+        val password:String = txtPassword.text.toString()
+
+        if(name.isNotEmpty() and email.isNotEmpty() and password.isNotEmpty()){
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this){
+                    task ->
+
+                    if(task.isComplete){
+                        val user:FirebaseUser ?= auth.currentUser
+                        verifyEmail(user)
+
+                        val userDB = dbReference.child(user?.uid.toString())
+
+                        userDB.child("Name").setValue(name)
+                        action()
+                    }
+                }
+        }
+        else{
+            Toast.makeText(this, "Fields can't be empty!", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun action(){
+        intent = Intent(this@SignUpActivity, ServerActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun verifyEmail(user:FirebaseUser?){
+        user?.sendEmailVerification()
+            ?.addOnCompleteListener(this){
+                task ->
+                if (task.isComplete){
+                    Toast.makeText(this, "Email Send", Toast.LENGTH_LONG).show()
+                }
+                else{
+                    Toast.makeText(this, "Error Sending Email", Toast.LENGTH_LONG).show()
+                }
+            }
     }
 }
